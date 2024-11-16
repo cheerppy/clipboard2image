f3863a2dcd1a003f4044e9f6d5d1bb859594ad73
cheerppy <47920686+cheerppy@users.noreply.github.com>
2024-11-16 12:47:05 +0900

    Add files via upload

---
+++ clip2jpg.bat
@@ -0,0 +1,19 @@
+@echo off
+setLocal
+
+set dirPath=%UserProfile%\OneDrive\‰æ‘œ
+set fileName=sample-image.jpg
+set filePath=%dirPath%\%fileName%
+
+set script=(Get-Clipboard -Format Image).Save("%filePath%",[System.Drawing.Imaging.ImageFormat]::Jpeg)
+set script=%script:"=\"%
+powershell -Command "%script%"
+
+if %ERRORLEVEL% neq 0 (
+	echo.
+	echo Press any key to exit . . .
+	pause > null
+	exit 1
+)
+
+explorer /select,"%filePath%"
